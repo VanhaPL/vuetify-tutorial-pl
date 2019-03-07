9966dda23ccc9a0c2986a9b3056dcb9c15ff12fe
Waldek <reclikmail@gmail.com>
2019-03-07 09:47:56 +0100

    DASHBOARD | chip

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -17,8 +17,9 @@
             <div>{{project.due}}</div>
           </v-flex>
           <v-flex xs2 sm4 md2>
-            <div class="caption grey--text">Status</div>
-            <div>{{project.status}}</div>
+            <div class="right">
+              <v-chip small :class="`${project.status} white--text`">{{project.status}}</v-chip>
+            </div>
           </v-flex>
         </v-layout>
         <v-divider></v-divider>
@@ -97,5 +98,17 @@ export default {
 
 .project.overdue {
   border-left: 4px solid tomato;
+}
+
+.v-chip.complete {
+  background: #3cd1c2;
+}
+
+.v-chip.ongoing {
+  background: orange;
+}
+
+.v-chip.overdue {
+  background: tomato;
 }
 </style>
