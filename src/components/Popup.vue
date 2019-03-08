23b355df7bab8a003788b63457110d521eb07a0d
Waldek <reclikmail@gmail.com>
2019-03-08 13:17:47 +0100

    DATE PICKER

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -10,6 +10,18 @@
           <!-- w Vue używamy pola v-text-field zamiast pola input  -->
           <v-text-field label="Title" v-model="title" prepend-icon="folder"></v-text-field>
           <v-textarea label="Information" v-model="content" prepend-icon="edit"></v-textarea>
+          <!-- date picker  -->
+          <v-menu>
+            <v-text-field
+              :value="formattedDate"
+              slot="activator"
+              label="Due date"
+              prepend-icon="date_range"
+            ></v-text-field>
+            <v-date-picker v-model="due"></v-date-picker>
+          </v-menu>
+          <!-- date picker  -->
+          <v-spacer></v-spacer>
           <v-btn flat class="success mx-0 mt-3" @click="submitka">Add project</v-btn>
         </v-form>
       </v-card-text>
@@ -18,16 +30,25 @@
 </template>
 
 <script>
+import format from "date-fns/format";
+
 export default {
   data() {
     return {
       title: "",
-      content: ""
+      content: "",
+      due: null
     };
   },
   methods: {
     submitka() {
+      // eslint-disable-next-line
       console.log(this.title, this.content);
+    }
+  },
+  computed: {
+    formattedDate() {
+      return this.due ? format(this.due, "do MMM YYYY") : "";
     }
   }
 };
